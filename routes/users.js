d86edf1f1b4968ad3971aacd6ae95afd43874868
Josh <zylo.codes@gmail.com>
2022-11-21 20:00:59 -0600

    User routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,48 @@
 const router = require('express').Router()
+const User = require('../models/users.js')
 
+router.get('/', async (req, res) => {
+  try {
+    const allUsers = await User.findAll()
+    res.json(allUsers)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
+router.post('/', async (req, res) => {
+  try {
+    const result = await User.create(req.body)
+    res.json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.put('/:user_id', async (req, res) => {
+  try {
+    const result = await User.update(req.body, {
+      where: {
+        id: req.params.user_id
+      }
+    })
+    res.json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.delete('/:user_id', async (req, res) => {
+  try {
+    const result = await User.destroy({
+      where: {
+        id: req.params.user_id
+      }
+    })
+    res.json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router
