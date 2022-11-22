7e97dc571fb931e77e05df974d02d9227b3cf69c
Josh <zylo.codes@gmail.com>
2022-11-21 19:19:32 -0600

    Movie routes GET and POST

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,76 @@
 const router = require('express').Router()
+const Movie = require('../models/movies.js')
 
+router.get('/', async (req, res) => {
+  try {
+    const allMovies = await Movie.findAll()
+    res.json(allMovies)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const result = await Movie.create(req.body)
+    res.json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.put('/:movie_id', async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.delete('/:movie_id', async (req, res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+
+//   // CREATE
+//   await Movie.create({ name: "Fido" })
+
+//   await Movie.bulkCreate([
+//     { name: "Inception" },
+//     { name: "Shawshank" },
+//     { name: "Forrest Gump" },
+//     { name: "Fight Club" },
+//     { name: "Ace Ventura: Pet Detective" },
+//     { name: "The Matrix" }
+//   ])
+
+
+//   // UPDATE
+//   await Movie.update({ name: "Star Wars" }, {
+//     where: {
+//       name: "Inception"
+//     }
+//   })
+
+//   await Movie.update({ name: "Six String Samurai"}, {
+//     where: {
+//       id: 3
+//     }
+//   })
+
+//   // DELETE
+//   await Movie.destroy({
+//     where: {
+//       id: 1
+//     }
+//   })
+
+//   const allMovies = await Movie.findAll()
+//   console.log(allMovies)
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./config/connection')
+const routes = require('./routes')
 
 const app = express()
 const PORT = process.env.PORT || 3001
 const isProduction = process.env.NODE_ENV === "production"
 
 app.use(express.json())
-
-
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`API server listening on http://localhost:${PORT}`)
@@ -16,63 +16,3 @@ app.listen(PORT, () => {
     console.log('Sequelize connected!')
   })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-// const Movie = require('./models/movies.js')
-
-// sequelize.sync({ force: true }).then(async () => {
-//   console.log('DB refreshed!')
-
-//   // CREATE
-//   await Movie.create({ name: "Fido" })
-
-//   await Movie.bulkCreate([
-//     { name: "Inception" },
-//     { name: "Shawshank" },
-//     { name: "Forrest Gump" },
-//     { name: "Fight Club" },
-//     { name: "Ace Ventura: Pet Detective" },
-//     { name: "The Matrix" }
-//   ])
-
-//   // READ
-//   // const allMovies = await Movie.findAll()
-//   // console.log(allMovies)
-//   // const fido = await Movie.findByPk(1)
-//   // console.log(fido)
-
-//   // UPDATE
-//   await Movie.update({ name: "Star Wars" }, {
-//     where: {
-//       name: "Inception"
-//     }
-//   })
-
-//   await Movie.update({ name: "Six String Samurai"}, {
-//     where: {
-//       id: 3
-//     }
-//   })
-
-//   // DELETE
-//   await Movie.destroy({
-//     where: {
-//       id: 1
-//     }
-//   })
-
-//   const allMovies = await Movie.findAll()
-//   console.log(allMovies)
-
-
-// })
